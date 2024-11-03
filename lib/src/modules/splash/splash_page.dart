a7d00aa9e021b924e3bdd4b8b9f429a949e50bae
LaysonEduardo <layson.contato@gmail.com>
2024-11-03 20:13:35 -0300

    changes: removed unecessary import

--- lib/src/modules/splash/splash_page.dart
+++ lib/src/modules/splash/splash_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dev_test/src/core/shared/data/state.dart';
 import 'package:flutter_dev_test/src/modules/splash/bloc/splash_bloc.dart';
 import 'package:flutter_dev_test/src/modules/splash/bloc/splash_event.dart';
 import 'package:flutter_modular/flutter_modular.dart';
